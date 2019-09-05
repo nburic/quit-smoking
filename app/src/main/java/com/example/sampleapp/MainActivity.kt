9d4b8b099af9d19dcf7a854511b5cf93d8fd93cb
Nejc Buric <nejc.buric@biokoda.com>
2019-09-05 10:30:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class -alMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

4544fd2790ba7cf925bb93eee3c302189c3c1502
Nejc Buric <nejc.buric@biokoda.com>
2019-09-11 14:34:44 +0200

    added custom view

---
+++ app/src/main/java/com/example/sampleapp/ProgressCardView.kt
@@ -0,0 +1,25 @@
+package com.example.sampleapp
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.cardview.widget.CardView
+
+class ProgressCardView : CardView {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        val view = View.inflate(context, R.layout.mp_card_progress, this)
+    }
+}

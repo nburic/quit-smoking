206b6a6fb859c5ce0344c373f766d81f6958c9ce
Nejc Buric <nejc.buric@biokoda.com>
2019-09-17 12:57:17 +0200

    added custom action bar with options

--- app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.sampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var toolbar: Toolbar
     private lateinit var viewPager: ViewPager
     private lateinit var tabLayout: TabLayout
 
@@ -15,13 +19,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         viewPager = findViewById(R.id.vp_main)
-        viewPager.adapter = viewPagerAdapter
+        viewPager.adapter = MainViewPagerAdapter(supportFragmentManager)
         viewPager.currentItem = 1
 
         tabLayout = findViewById(R.id.tl_main)
         tabLayout.setupWithViewPager(viewPager)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_settings, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item?.itemId) {
+            R.id.mi_settings -> Log.d("!!!", "Settings clicked...")
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }

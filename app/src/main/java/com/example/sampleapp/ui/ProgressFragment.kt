49b8f18df1f2478f9b7c8a24ad639a3203c1ef54
Nejc Buric <nejc.buric@biokoda.com>
2019-11-04 16:16:49 +0100

    added action image for setting goal

--- app/src/main/java/com/example/sampleapp/ui/ProgressFragment.kt
+++ app/src/main/java/com/example/sampleapp/ui/ProgressFragment.kt
@@ -1,6 +1,7 @@
 package com.example.sampleapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -60,6 +61,9 @@ class ProgressFragment : Fragment() {
         }
 
         progressCardView = view.findViewById(R.id.progressCard)
+        progressCardView.ivSetGoal.setOnClickListener {
+            Log.d("!!!", "Open bottom sheet dialog to set goal")
+        }
 
         viewModel.user.observe(this, userObserver)
 
--- app/src/main/java/com/example/sampleapp/views/ProgressCardView.kt
+++ app/src/main/java/com/example/sampleapp/views/ProgressCardView.kt
@@ -3,13 +3,16 @@ package com.example.sampleapp.views
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import com.example.sampleapp.R
 
 class ProgressCardView : CardView {
 
-    lateinit var tvProgressValue: TextView
+    private lateinit var tvProgressValue: TextView
+    lateinit var ivSetGoal: ImageView
+
 
     constructor(context: Context) : super(context) {
         init()
@@ -27,6 +30,7 @@ class ProgressCardView : CardView {
         val view = View.inflate(context, R.layout.mp_card_progress, this)
 
         tvProgressValue = view.findViewById(R.id.tv_progress_value)
+        ivSetGoal = view.findViewById(R.id.iv_set_goal)
     }
 
     fun setProgressValue(value: String?) {

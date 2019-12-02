86dd009cc6b4bc0d4d36e38111c5aeef7022fb41
Nejc Buric <nejc.buric@biokoda.com>
2019-12-02 15:01:55 +0100

    job service

--- app/src/main/java/com/example/sampleapp/MainApplication.kt
+++ app/src/main/java/com/example/sampleapp/MainApplication.kt
@@ -1,24 +1,11 @@
 package com.example.sampleapp
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.graphics.Color
-import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.core.app.NotificationCompat
 import com.example.sampleapp.db.AppDatabase
 import com.example.sampleapp.repo.AppRepo
 import timber.log.Timber
-import android.content.Intent
-import android.app.PendingIntent
 
 class MainApplication: Application() {
-
-    companion object {
-        const val NOTIFICATION_CHANNEL_ID = "notification_channel_id"
-        const val NOTIFICATION_ID = 10000
-    }
 
     private lateinit var repository: AppRepo
 
@@ -28,34 +15,5 @@ class MainApplication: Application() {
         Timber.plant(Timber.DebugTree())
 
         repository = AppRepo(AppDatabase.getDatabase(this).userDao())
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            createNotificationChannel()
-
-            val notificationIntent = Intent(this, MainActivity::class.java)
-            val notificationPendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-            val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
-                .setContentTitle("[Goal achieved!]")
-                .setContentText("[Smoke free for 1 year]")
-                .setSmallIcon(R.drawable.ic_notification_goal_done)
-                .setContentIntent(notificationPendingIntent)
-                .setAutoCancel(true)
-                .build()
-
-            val manager = getSystemService(NotificationManager::class.java)
-            manager.notify(NOTIFICATION_ID, notification)
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun createNotificationChannel() {
-        val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, "[CHANNEL GOAL]", NotificationManager.IMPORTANCE_HIGH)
-        channel.description = "[This is channel goal]"
-        channel.enableLights(true)
-        channel.lightColor = Color.YELLOW
-
-        val manager = getSystemService(NotificationManager::class.java)
-        manager.createNotificationChannel(channel)
     }
 }
--- app/src/main/java/com/example/sampleapp/services/NotificationJobService.kt
+++ app/src/main/java/com/example/sampleapp/services/NotificationJobService.kt
@@ -1,15 +1,62 @@
 package com.example.sampleapp.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.example.sampleapp.MainActivity
+import com.example.sampleapp.MainApplication
+import com.example.sampleapp.R
 
 
 class NotificationJobService: JobService() {
+
+    companion object {
+        const val NOTIFICATION_CHANNEL_ID = "notification_channel_id"
+        const val NOTIFICATION_ID = 10000
+    }
+
+    private lateinit var notifyManager: NotificationManager
+
     override fun onStopJob(params: JobParameters?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return true
     }
 
     override fun onStartJob(params: JobParameters?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        createNotificationChannel()
+
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val notificationPendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("[Goal achieved!]")
+            .setContentText("[Smoke free for 1 year]")
+            .setSmallIcon(R.drawable.ic_notification_goal_done)
+            .setContentIntent(notificationPendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        notifyManager.notify(NOTIFICATION_ID, notification)
+
+        return false
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notifyManager = getSystemService(NotificationManager::class.java)
+
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, "[CHANNEL GOAL]", NotificationManager.IMPORTANCE_HIGH)
+            channel.description = "[This is channel goal]"
+            channel.enableLights(true)
+            channel.lightColor = Color.YELLOW
+
+            notifyManager.createNotificationChannel(channel)
+        }
     }
 }
